e12fb616d18e716d12f2833af563c1e5afea806b
workmin <workmin0929@gmail.com>
2022-03-25 17:55:24 +0900

    init

--- src/main/java/com/spcgSM/thymeleafTEST/controller/BoardController.java
+++ src/main/java/com/spcgSM/thymeleafTEST/controller/BoardController.java
@@ -5,9 +5,10 @@ import com.spcgSM.thymeleafTEST.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,5 +23,25 @@ public class BoardController {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards",boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null){
+            model.addAttribute("board",new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/spcgSM/thymeleafTEST/model/Board.java
+++ src/main/java/com/spcgSM/thymeleafTEST/model/Board.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Data
@@ -13,7 +16,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30)
     private String title;
     private String Content;
-
 }
